7e87452e98edcc826673ae7f437e7405121bc78e
Daniel W. Hieber <dwhieb@gmail.com>
2018-10-31 12:00:33 -0500

    setup tests

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Accepts a FLEx XML export (FlexText format) as a String and converts it to an Array of DLx Text Objects
+ * @param  {String} xml The FLEx XML export to convert
+ * @return {Array}
+ */
+function flextext2dlx(xml) {
+  return [];
+}
+
+module.exports = flextext2dlx;
